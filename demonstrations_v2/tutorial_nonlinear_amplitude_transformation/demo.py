335df76e90441b819cdfdfcbfca55f4660d70ba7
drdren <104710745+drdren@users.noreply.github.com>
2026-04-08 13:01:41 -0400

    Update demonstrations_v2/tutorial_nonlinear_amplitude_transformation/demo.py

--- demonstrations_v2/tutorial_nonlinear_amplitude_transformation/demo.py
+++ demonstrations_v2/tutorial_nonlinear_amplitude_transformation/demo.py
@@ -165,7 +165,6 @@ r"""#Nonlinear amplitude transformation
 # we move on to applying QSVT polynomials.
 # 
 
-pip install pennylane pyqsp -q
 
 import pennylane as qml
 from pyqsp.poly import PolyTaylorSeries
