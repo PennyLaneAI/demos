c6695a942796bba1431002c2621a866c963bb540
qottmann <korbinian.kottmann@gmail.com>
2026-06-18 11:03:19 +0200

    update

--- demonstrations_v2/tutorial_surface_code/demo.py
+++ demonstrations_v2/tutorial_surface_code/demo.py
@@ -27,7 +27,32 @@ variant of the `Kitaev surface code <https://errorcorrectionzoo.org/c/surface>`_
 with the :doc:`toric code <demos/tutorial_toric_code>` or one of the `many other variants <https://errorcorrectionzoo.org/list/quantum_surface>`__
 that generalize it.
 
-In particular and most notably, we want to stress the difference to the original planar surface code:
+The rotated surface code consists of alternating X (blue) and Z (orange) squares that make up a lattice.
+The so-called data qubits sit on the vertices (highlighted in pink). These will be the qubits that encode 
+the quantum information of the 5x5 patch that is making up a single qubit:
+
+.. figure:: ../_static/demonstration_assets/surface_code/surface_code_syndrome.png
+    :align: center
+    :width: 50%
+    :target: javascript:void(0)
+    
+We additionally have syndrome qubits (in light blue) that sit in the middle of the squares. 
+These syndrome qubits are used to continuously perform measurements on the surrounding data qubits in a non-destructive way:
+stabilizers.
+We have :math:`X`- and :math:`Z`-stabilizers with either four or two operators on the edges.
+
+.. figure:: ../_static/demonstration_assets/surface_code/surface_code_with_stabilizers.png
+    :align: center
+    :width: 50%
+    :target: javascript:void(0)
+
+These stabilizers are measured by entangling the data qubits via :math:`\text{CNOT}` gates with the corresponding syndrome qubit and then
+measuring that. The measurement result :math:`\pm 1` of a stabilizer measurement indicates whether or not an error has occured.
+A :math:`d \times d` surface code qubit has code distance :math:`d`, meaning that it can correct up to :math:`d` errors. When more errors occur,
+it may go unnoticed as all stabilizers give a false positive result.
+
+We now want to go into more detail an expand on each of these components. 
+Before that, we want to stress the difference of the *rotated* surface code to the original planar surface code:
 
 .. figure:: ../_static/demonstration_assets/lattice_surgery/surface_code_qubit2.png
     :align: center
@@ -44,20 +69,22 @@ Stabilizers
 
 The surface code is a stabilizer code, which means that quantum information is encoded in the joint :math:`+1` eigenspace
 of a set of commuting observables :math:`\mathcal{S}` called *stabilizers*. These stabilizers :math:`S_i \in \mathcal{S}` 
-are faces of four :math:`Z` or :math:`X` observables, arranged in a checkerboard formation:
+are faces of four or two :math:`Z` or :math:`X` observables, arranged in a checkerboard formation:
 
-.. figure:: ../_static/demonstration_assets/surface_code/stabilizers.png
+.. figure:: ../_static/demonstration_assets/surface_code/surface_code_with_stabilizers.png
     :align: center
     :width: 50%
     :target: javascript:void(0)
     
-All these stabilizers commute with each other (that is one the defining properties). That is because generally Pauli words commute when
-they overlap on an equal number of sites, and, in particular for the most simple case, :math:`[XX, ZZ] = 0`.
+All these stabilizers commute with each other - that is one the defining properties. 
+That all stabilizers in the surface code depicted here commute can be seen from the fact that generally Pauli words commute when
+they overlap on an equal number of sites. Take for example the most simple case, two sites ``[0, 1]``, we have :math:`[X_0 X_1, Z_0 Z_1] = 0`.
 
 The measurement outcome of such a stabilizer is binary :math:`\pm 1` and we assume that the underlying quantum state
-:math:`|\psi\rangle` that is encoded by the qubit of the surface code is in the logical subspace, i.e. the joint 
-:math:`+1` eigenspace or the so-called code space. That means that
-the action of any stabilizer :math:`S_i \in \mathcal{S}` is equal to the identity, :math:`S_i |\psi\rangle = + 1 |\psi\rangle`.
+:math:`|\psi\rangle` that is encoded by the qubit of the surface code is in the so-called code (sub)space - the joint 
+:math:`+1` eigenspace of all stabilizer measurements.
+That means that the action of any stabilizer :math:`S_i \in \mathcal{S}` on a state :math:`|\psi\rangle` in the code space 
+is equal to the identity, :math:`S_i |\psi\rangle = + 1 |\psi\rangle`.
 
 By continuously measuring all stabilizers we can ensure that the state is not leaving the code space. 
 If, however, we do measure :math:`-1` somewhere, we know that an error has occured. In that case, we need to perform
