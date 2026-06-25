33c09fde4188fd699a5f0b4f8cfc0f8dd2159406
Rishabh Gupta <78001292+rishabh-gupta28@users.noreply.github.com>
2026-06-25 15:29:41 -0400

    Apply suggestion from @drdren
    Co-authored-by: drdren <104710745+drdren@users.noreply.github.com>

--- demonstrations_v2/tutorial_nonlinear_amplitude_transformation/demo.py
+++ demonstrations_v2/tutorial_nonlinear_amplitude_transformation/demo.py
@@ -702,7 +702,7 @@ accuracy(best_weight, data, labels)
 # 
 # In this demo, we have implemented the nonlinear amplitude transformation described in Guo et
 # al. (2024) and Rattew and Rebentrost (2024) [#ntca]_,
-# `4 <https://arxiv.org/abs/2309.09839>`__]. We verified the diagonal amplitude block encoding on a
+# [#importancesampling]_. We verified the diagonal amplitude block encoding on a
 # toy example, applied a :math:`\tanh` nonlinearity via QSVT, and integrated the activation as a layer
 # inside a small quantum classifier trained on downscaled MNIST.
 # 
